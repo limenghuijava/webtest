62d7d1e1b388c3db4960132090ce4a6a0dbc6694
leyuan.lv <leyuan.lv@qunar.com>
2018-01-17 03:25:36 +0800

    第一次提交需要手动选择要提交的内容,后续会自动过滤.gitignore中配置的过滤文件

---
+++ src/com/lbb/servlet/start/StartServlet.java
@@ -0,0 +1,41 @@
+package com.lbb.servlet.start;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 开始
+ *
+ * @author Kevin
+ * @create 2018-01-17 2:21
+ */
+
+public class StartServlet extends HttpServlet{
+
+    private static final long serialVersionUID = -4724888703643052018L;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String join = "";
+        while (req.getHeaderNames().hasMoreElements()){
+            join += req.getHeaderNames().nextElement();
+        }
+        String p1 = req.getParameter("lbb");
+        PrintWriter pw = new PrintWriter(resp.getOutputStream());
+        pw.append(p1);
+        pw.append("--");
+        pw.append(join);
+        pw.println();
+        pw.close();
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String protocol = req.getProtocol();
+        System.out.println(protocol);
+        this.doGet(req,resp);
+    }
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Kevin
+  Date: 2018/1/17
+  Time: 2:14
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>我家有个胖包</title>
+  </head>
+  <body>
+  <h1> 现在是凌晨两点十九分,李宝宝睡得很香</h1>
+  </body>
+</html>
